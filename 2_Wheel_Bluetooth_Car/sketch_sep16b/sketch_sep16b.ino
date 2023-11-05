075d92ec260e829beb18867e505abcbfcf139825
shivam361v <123078985+shivam361v@users.noreply.github.com>
2023-11-05 19:53:17 +0530

    done

--- 2_Wheel_Bluetooth_Car/sketch_sep16b/sketch_sep16b.ino
+++ 2_Wheel_Bluetooth_Car/sketch_sep16b/sketch_sep16b.ino
@@ -1,82 +1,142 @@
+#include <Servo.h> //Servo motor library
+#include <NewPing.h> //Ultrasonic sensor function library
 
-// Starting of Program
-int m1a = 9;
-int m1b = 10;
-int m2a = 11;
-int m2b = 12;
-char val;
+const int LeftForward = 11;// L298N control pins
+const int LeftBackward = 10;
+const int RightForward = 13;
+const int RightBackward = 12;
 
-void setup() {
-  pinMode(m1a, OUTPUT);  // Digital pin 10 set as output Pin
-  pinMode(m1b, OUTPUT);  // Digital pin 11 set as output Pin
-  pinMode(m2a, OUTPUT);  // Digital pin 12 set as output Pin
-  pinMode(m2b, OUTPUT);  // Digital pin 13 set as output Pin
-  Serial.begin(9600);
+#define trig_pin A1 //sensor pins - analog input 1
+#define echo_pin A2 //analog input 2
+#define maximum_distance 200
+
+boolean goesForward = false;
+int distance = 100;
+
+NewPing sonar(trig_pin, echo_pin, maximum_distance); //sensor function
+Servo servo_motor; //servo name
+
+void setup(){
+  pinMode(RightForward, OUTPUT);
+  pinMode(LeftForward, OUTPUT);
+  pinMode(LeftBackward, OUTPUT);
+  pinMode(RightBackward, OUTPUT);
+  servo_motor.attach(7); //servo pin
+  servo_motor.write(115);
+  delay(2000);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
 }
 
-void loop() {
-  while (Serial.available() > 0) {
-    val = Serial.read();
-    Serial.println(val);
+void loop(){
+  int distanceRight = 0;
+  int distanceLeft = 0;
+  delay(50);
+   if (distance <= 20){
+    moveStop(); // obstacle probably on the route forward, so stop
+    delay(300);
+    moveBackward();
+    delay(400);
+    moveStop();
+    delay(300);
+    distanceRight = lookRight();
+    delay(300);
+    distanceLeft = lookLeft();
+    delay(300);
+    if (distance >= distanceLeft){
+      turnRight(); // calculate in which direction the obstacle is more far
+      moveStop();
+    }
+    else{
+      turnLeft();
+      moveStop();
+    }
   }
+  else{
+    moveForward(); 
+  }
+    distance = readPing();
+}
 
-  if (val == 'F')  // Forward
-  {
-    digitalWrite(m1a, HIGH);
-    digitalWrite(m1b, LOW);
-    digitalWrite(m2a, HIGH);
-    digitalWrite(m2b, LOW);
-  } else if (val == 'B')  // Backward
-  {
-    digitalWrite(m1a, LOW);
-    digitalWrite(m1b, HIGH);
-    digitalWrite(m2a, LOW);
-    digitalWrite(m2b, HIGH);
-  }
+int lookRight(){  
+  servo_motor.write(50);
+  delay(500);
+  int distance = readPing();
+  delay(100);
+  servo_motor.write(115);
+  return distance;
+}
 
-  else if (val == 'L')  //Left
-  {
-    digitalWrite(m1a, LOW);
-    digitalWrite(m1b, LOW);
-    digitalWrite(m2a, HIGH);
-    digitalWrite(m2b, LOW);
-  } else if (val == 'R')  //Right
-  {
-    digitalWrite(m1a, HIGH);
-    digitalWrite(m1b, LOW);
-    digitalWrite(m2a, LOW);
-    digitalWrite(m2b, LOW);
-  }
+int lookLeft(){
+  servo_motor.write(170);
+  delay(500);
+  int distance = readPing();
+  delay(100);
+  servo_motor.write(115);
+  return distance;
+  delay(100);
+}
 
-  else if (val == 'S')  //Stop
-  {
-    digitalWrite(m1a, LOW);
-    digitalWrite(m1b, LOW);
-    digitalWrite(m2a, LOW);
-    digitalWrite(m2b, LOW);
-  } else if (val == 'I')  //Forward Right
-  {
-    digitalWrite(m1a, HIGH);
-    digitalWrite(m1b, LOW);
-    digitalWrite(m2a, LOW);
-    digitalWrite(m2b, LOW);
-  } else if (val == 'J')  //Backward Right
-  {
-    digitalWrite(m1a, LOW);
-    digitalWrite(m1b, HIGH);
-    digitalWrite(m2a, LOW);
-    digitalWrite(m2b, LOW);
-  } else if (val == 'G')  //Forward Left
-  {
-    digitalWrite(m1a, LOW);
-    digitalWrite(m1b, LOW);
-    digitalWrite(m2a, HIGH);
-    digitalWrite(m2b, LOW);
-  } else if (val == 'H')  //Backward Left
-  {
-    digitalWrite(m1a, LOW);
-    digitalWrite(m1b, LOW);
-    digitalWrite(m2a, LOW);
-    digitalWrite(m2b, HIGH);
+int readPing(){
+  delay(70);
+  int cm = sonar.ping_cm();
+  if (cm==0){
+    cm=250;
   }
+  return cm;
+}
+
+void moveStop(){
+  digitalWrite(RightForward, LOW);
+  digitalWrite(LeftForward, LOW);
+  digitalWrite(RightBackward, LOW);
+  digitalWrite(LeftBackward, LOW);
+}
+
+void moveForward(){
+  if(!goesForward){
+    goesForward=true;
+    digitalWrite(LeftForward, HIGH);
+    digitalWrite(RightForward, HIGH);
+    digitalWrite(LeftBackward, LOW);
+    digitalWrite(RightBackward, LOW); 
+  }
+}
+
+void moveBackward(){
+  goesForward=false;
+  digitalWrite(LeftBackward, HIGH);
+  digitalWrite(RightBackward, HIGH);
+  digitalWrite(LeftForward, LOW);
+  digitalWrite(RightForward, LOW);
+}
+
+void turnRight(){
+  digitalWrite(LeftForward, HIGH);
+  digitalWrite(RightBackward, HIGH);
+  digitalWrite(LeftBackward, LOW);
+  digitalWrite(RightForward, LOW);
+  delay(500);
+  digitalWrite(LeftForward, HIGH);
+  digitalWrite(RightForward, HIGH);
+  digitalWrite(LeftBackward, LOW);
+  digitalWrite(RightBackward, LOW);
+}
+
+void turnLeft(){
+  digitalWrite(LeftBackward, HIGH);
+  digitalWrite(RightForward, HIGH);
+  digitalWrite(LeftForward, LOW);
+  digitalWrite(RightBackward, LOW);
+  delay(500);
+  digitalWrite(LeftForward, HIGH);
+  digitalWrite(RightForward, HIGH);
+  digitalWrite(LeftBackward, LOW);
+  digitalWrite(RightBackward, LOW);
 }
